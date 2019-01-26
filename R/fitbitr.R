d3aa9b6d5df1faadf68d1691b31c70f980dca33e
Ernesto Ramirez <er.ramirez@gmail.com>
2019-01-26 09:59:38 -0800

    add v1.2 for sleep

--- R/fitbitr.R
+++ R/fitbitr.R
@@ -5,4 +5,6 @@
 #' @name fitbitr
 #' @docType package
 url_base <- "https://api.fitbit.com/1/"
+url_sleep <- "https://api.fitbit.com/1.2/"
 url_api  <- paste0(url_base, "user/-/")
+url_api_sleep <- paste0(url_sleep, "user/-/")
